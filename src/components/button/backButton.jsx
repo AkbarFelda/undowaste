7596ea866cfa3a1d877090b5eef70059f3acb670
AkbarFelda <akbarfelda65@gmail.com>
2025-11-17 20:50:10 +0700

    feat: [AF] add NotFound page with decorative blobs and back button

---
+++ src/components/button/backButton.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+// IMPORT UTAMA: Komponen untuk merender ikon
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
+function BackButton() {
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <button
+      onClick={handleGoBack}
+      className="mt-4 px-6 py-2 bg-white text-background-notfound rounded-full font-bold shadow-lg flex items-center gap-2 z-10 hover:bg-gray-100 transition duration-300"
+    >
+      {/* Ganti text-white dengan text-background-notfound agar warna ikon terlihat 
+          (karena latar belakang tombol adalah putih) */}
+      <FontAwesomeIcon 
+        icon={faArrowLeft} 
+        className="text-background-notfound text-base" 
+      />
+      Kembali
+    </button>
+  );
+}
+
+export default BackButton;
