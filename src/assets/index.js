612b7b87e6fd47dc33262a2fc2c57d72ba39cd68
ratih shinta <rstihshintaaa6@gmail.com>
2025-11-26 20:21:00 +0700

    feat [RS] : make about page

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,11 @@ import BlobR from './icons/BlobR.svg';
 import Icon404 from './icons/404.svg';
 import UndowasteLogo from './icons/UndowasteLogo.svg';
 import TrashHero from './images/trash.svg';
+import Bottle from './images/bottle.svg';
+import Edukasi from './images/edukasi.svg';
+import Keberlanjutan from './images/keberlanjutan.svg';
+import Kolaborasi from './images/kolaborasi.svg';
+import Magnifier from './images/magnifier.svg'; 
 
 
 export { 
@@ -10,5 +15,10 @@ export {
   BlobR,
   Icon404,
   UndowasteLogo,
-  TrashHero
+  TrashHero,
+  Bottle,
+  Edukasi,
+  Keberlanjutan,
+  Kolaborasi,
+  Magnifier
 };
