5633b2978d2c133bfd8f35161e9807ec7e324578
ratih shinta <rstihshintaaa6@gmail.com>
2025-11-28 19:37:10 +0700

    style [RS] : update responsive

--- src/components/card/content/RecycleSection.jsx
+++ src/components/card/content/RecycleSection.jsx
@@ -3,11 +3,10 @@ import { Boxes } from "../../../assets";
 
 export default function RecycleSection() {
   return (
-    <div className="bg-[#FAFAFA] rounded-[65px] shadow-lg p-14 flex flex-col md:flex-row gap-[124px] items-center">
-      
+    <div className="bg-[#FAFAFA] rounded-3xl md:rounded-[65px] shadow-lg p-5 lg:p-14 flex flex-col md:flex-row gap-7 lg:gap-[124px] items-start justify-center">
       <div className="flex-1">
-        <h2 className="text-5xl font-leaguespartansemibold text-black mb-5">Recycle</h2>
-        <p className="font-leaguespartanmedium text-xl text-black leading-[38px] mb-6 text-justify w-5/6">
+        <h2 className="text-3xl lg:text-5xl font-leaguespartansemibold text-black mb-5">Recycle</h2>
+        <p className="font-leaguespartanmedium text-sm lg:text-xl text-black leading-5 lg:leading-[38px] mb-6 text-justify w-5/6">
           Recycle berarti mengolah kembali barang atau bahan bekas menjadi produk baru agar dapat digunakan kembali.
         </p>
         <img 
@@ -17,13 +16,13 @@ export default function RecycleSection() {
         />
       </div>
 
-      <div className="flex-1 bg-[#527133] rounded-[50px] py-14 px-18 pr-48">
-        <h3 className="font-leaguespartanmedium text-white leading-10 text-[40px] mb-[30px]">
+      <div className="flex-1 bg-[#527133] rounded-3xl md:rounded-[50px] py-8 lg:py-14 px-10 lg:px-18 lg:pr-48">
+        <h3 className="font-leaguespartanmedium text-white leading-8 lg:leading-10 text-2xl lg:text-[40px] mb-5 lg:mb-[30px]">
           Actionable 
           <br />
           Guides and Tips
         </h3>
-        <ul className="list-disc pl-5 space-y-2 text-white font-poppinsmedium leading-10 text-2xl">
+        <ul className="list-disc pl-5 space-y-2 text-white font-poppinsmedium leading-5 lg:leading-10 text-sm lg:text-2xl">
           <li>Cuci & Keringkan Wadah Bekas</li>
           <li>Pilah Sesuai Jenis Material</li>
           <li>Ikuti Kode Segitiga Plastik</li>
